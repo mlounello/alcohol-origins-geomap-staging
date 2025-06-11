0f24e4a79bbc8870d6ffeb4746cecaba39f66e00
Michael Lounello <mlounello@siena.edu>
2025-06-10 21:52:56 -0400

    chore: auto-commit all changes before push

--- create_map.py
+++ create_map.py
@@ -207,7 +207,7 @@ def create_folium_map(df: pd.DataFrame) -> folium.Map:
             folium.PolyLine(
                 locations=[coords[pid], (row["latitude"], row["longitude"])],
                 color=group_color_map[grp],
-                weight=5, opacity=0
+                weight=5, opacity=100
             ).add_to(fg)
         folium.CircleMarker(
             location=[row["latitude"], row["longitude"]],
